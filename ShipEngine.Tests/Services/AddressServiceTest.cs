a1a89c3480674d727989b30289fd22b8519a9b9d
Seth Silesky <seth.silesky@gmail.com>
2021-03-09 23:40:55 -0600

    update address service tests

--- ShipEngine.Tests/Services/AddressServiceTest.cs
+++ ShipEngine.Tests/Services/AddressServiceTest.cs
@@ -1,8 +1,7 @@
 using NUnit.Framework;
 using ShipEngine.Extensions;
-using ShipEngine.Models;
 using ShipEngine.Models.Address.Dto;
-using System;
+using ShipEngine.Models.Domain;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
@@ -10,22 +9,39 @@ namespace ShipEngine.Tests
 {
     public class AddressService : BaseShipEngineServiceTest
     {
-        [SetUp]
+        AddressValidationParams AddressValidationParamsFixture = new AddressValidationParams
+        {
+            Street = new List<string> { "One", "Two", "Three" },
+            CityLocality = "Chicago",
+            CountryCode = "US",
+            PostalCode = "60613",
+            StateProvince = "IL",
+        };
 
         [Test]
         public async Task AddressValidationHighLevelTest()
         {
-            AddressValidationParams p = new AddressValidationParams
-            {
-                Street = new List<string> { "One", "Two", "Three" },
-                CityLocality = "Chicago",
-                CountryCode = "US",
-                PostalCode = "60613",
-                StateProvince = "IL",
-            };
+            AddressValidationResult validationResult = await MockShipEngineClient.Address.Validate(AddressValidationParamsFixture);
+            Assert.That(validationResult.Address.CountryCode, Is.EqualTo("US"));
+            Assert.That(validationResult.Valid, Is.InstanceOf<bool>());
+            Assert.That(validationResult.Messages.Errors, Is.InstanceOf<List<string>>());
+            Assert.That(validationResult.Messages.Info, Is.InstanceOf<List<string>>());
+            Assert.That(validationResult.Messages.Warnings, Is.InstanceOf<List<string>>());
+        }
 
-            var validationResult = await MockShipEngineClient.Address.Validate(p);
-            Assert.AreEqual(validationResult.Address.CountryCode, "US");
+        [Test]
+        public async Task AddressValidationConvenienceTest()
+        {
+            var p = AddressValidationParamsFixture;
+            Address validationResult = await MockShipEngineClient.ValidateAddress(
+                street: p.Street, cityLocality: p.CityLocality, countryCode: p.CityLocality,
+                postalCode: p.PostalCode, stateProvince: p.StateProvince
+            );
+            Assert.That(validationResult.Residential, Is.InstanceOf<bool>());
+            Assert.That(validationResult.PostalCode, Is.InstanceOf<string>());
+            Assert.That(validationResult.CountryCode, Is.EqualTo("US"));
+            Assert.That(validationResult.Residential, Is.InstanceOf<bool>());
+            Assert.That(validationResult.Street, Is.InstanceOf<List<string>>());
         }
     }
 }
