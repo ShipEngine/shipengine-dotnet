6a42d4b4ca31de583bcf6f1b0cecc7f1e17fd8af
Randall <randall.krauskopf@gmail.com>
2021-08-05 15:47:26 -0500

    W.I.P.

--- ShipEngine.Tests/ShipEngineMethods/AddressValidationTest.cs
+++ ShipEngine.Tests/ShipEngineMethods/AddressValidationTest.cs
@@ -57,11 +57,40 @@ namespace ShipEngineTest
             Assert.Equal("CA", result[0].OriginalAddress.CountryCode);
             Assert.Equal("unknown", result[0].OriginalAddress.AddressResidentialIndicator);
 
-
             Assert.Equal("a1007", result[0].Messages[0].Code);
             Assert.Equal("verified_to_suite_level", result[0].Messages[0].DetailCode);
             Assert.Equal("This address has been verified down to the suite/PO box level (highest possible accuracy with the provided data)", result[0].Messages[0].Message);
             Assert.Equal("info", result[0].Messages[0].Type);
+        }
+
+        [Fact]
+        public async void InValidAddressTest()
+        {
+            var mockHttpClientFixture = new MockHttpClientFixture();
+
+            var json = "{  \"request_id\": \"22467317-a130-4927-95a6-76124b716e58\",  \"errors\": [    {      \"error_source\": \"shipengine\",      \"error_type\": \"validation\",      \"error_code\": \"request_body_required\",      \"message\": \"Request body cannot be empty.\"    }  ]}";
+            mockHttpClientFixture.StubRequest(HttpMethod.Post, "/v1/addresses/validate", System.Net.HttpStatusCode.BadRequest, json);
+
+            var addressList = new List<Address>(){
+                new Address() {
+                    AddressLine1 = "2 Toronto St",
+                    CityLocality = "Toronto",
+                    StateProvince = "ON",
+                    PostalCode = "M5C 2B5",
+                    CountryCode = "CA",
+                }
+            };
+
+            var client = new ShipEngine("TEST_bTYAskEX6tD7vv6u/cZ/M4LaUSWBJ219+8S1jgFcnkk", mockHttpClientFixture.HttpClient);
+
+
+            var ex = await Assert.ThrowsAsync<ShipEngineException>(async () => await client.ValidateAddresses(addressList));
+
+            Assert.Equal("22467317-a130-4927-95a6-76124b716e58", ex.RequestId);
+            Assert.Equal("shipengine", ex.Errors[0].ErrorSource);
+            Assert.Equal("validation", ex.Errors[0].ErrorType);
+            Assert.Equal("request_body_required", ex.Errors[0].ErrorCode);
+            Assert.Equal("validation", ex.Errors[0].ErrorType);
         }
     }
 }
