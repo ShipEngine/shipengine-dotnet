fa80cc8907435ea612cd39249d03fbe4c4aa38eb
rkrauskopf <randall.krauskopf@gmail.com>
2021-08-12 09:53:46 -0500

    W.I.P.

--- ShipEngine/Constants/ErrorSource.cs
+++ ShipEngine/Constants/ErrorSource.cs
@@ -3,9 +3,7 @@ namespace ShipEngineSDK
     public static class ErrorSource
     {
         public const string ShipEngine = "shipengine";
-
         public const string Carrier = "carrier";
-
         public const string OrderSource = "order_source";
     }
 }
