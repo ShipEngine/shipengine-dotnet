66e2264a225ae6818f43884c29c7f908bc41cbac
Randall Krauskopf <randall.krauskopf@auctane.com>
2021-08-25 11:58:27 -0500

    Track Using Carrier Code and Tracking Number (#29)
    Added enums, C# comments, got unit tests working.

--- ShipEngine.Tests/ShipEngineMethodTests/TrackUsingCarrierCodeAndTrackingNumberTest.cs
+++ ShipEngine.Tests/ShipEngineMethodTests/TrackUsingCarrierCodeAndTrackingNumberTest.cs
@@ -1,54 +1,138 @@
+using Moq;
+using Moq.Protected;
+using Newtonsoft.Json;
 using ShipEngineSDK;
+using ShipEngineSDK.TrackUsingCarrierCodeAndTrackingNumber;
+using ShipEngineSDK.TrackUsingCarrierCodeAndTrackingNumber.Result;
+using System;
 using System.IO;
 using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace ShipEngineTest
 {
     public class TrackUsingCarrierCodeAndTrackingNumberTest
     {
+        public TestUtils TestUtils;
 
-        // TODO: figure out how to get mock to match on route with query string
-        // [Fact]
+        public TrackUsingCarrierCodeAndTrackingNumberTest()
+        {
+            TestUtils = new TestUtils();
+        }
 
-        // public async void ValidTrackUsingCarrierCodeAndTrackingNumberTest()
-        // {
-        //     var mockHttpClientFixture = new MockHttpClientFixture();
+        [Fact]
+        public async void ValidTrackUsingCarrierCodeAndTrackingNumberTest()
+        {
+            var config = new Config("TEST_bTYAskEX6tD7vv6u/cZ/M4LaUSWBJ219+8S1jgFcnkk");
+            var mockShipEngineFixture = new MockShipEngineFixture(config);
 
-        //     string json = File.ReadAllText(Path.Combine(Directory.GetCurrentDirectory(), "../../../HttpResponseMocks/TrackUsingCarrierCodeAndTrackingNumber200Response.json"));
+            string json = File.ReadAllText(
+                Path.Combine(Directory.GetCurrentDirectory(),
+                "../../../HttpResponseMocks/TrackUsingCarrierCodeAndTrackingNumber200Response.json")
+            );
 
-        //     mockHttpClientFixture.StubRequest(HttpMethod.Get, "/v1/tracking?tracking_number=1026167028310292&carrier_code=usps", System.Net.HttpStatusCode.OK, json);
+            var responseMessage = new HttpResponseMessage(System.Net.HttpStatusCode.OK);
+            responseMessage.Content = new StringContent(json);
 
-        //     var client = new ShipEngine("TEST_bTYAskEX6tD7vv6u/cZ/M4LaUSWBJ219+8S1jgFcnkk", mockHttpClientFixture.HttpClient);
+            mockShipEngineFixture.MockHandler.Protected()
+                .Setup<Task<HttpResponseMessage>>(
+                    "SendAsync",
+                    ItExpr.Is<HttpRequestMessage>(m =>
+                        m.Method == HttpMethod.Get &&
+                        m.RequestUri.AbsolutePath.Contains("/v1/tracking")),
+                    ItExpr.IsAny<CancellationToken>())
+                .Returns(Task.FromResult(responseMessage));
 
-        //     var result = await client.TrackUsingCarrierCodeAndTrackingNumber("1026167028310292", "usps");
+            var result = await mockShipEngineFixture.ShipEngine.TrackUsingCarrierCodeAndTrackingNumber("1026167028310292", "usps");
 
-        //     Assert.Equal("1026167028310292", result.TrackingNumber);
-        //     Assert.Equal("UN", result.StatusCode);
-        //     Assert.Null(result.CarrierDetailCode);
-        //     Assert.Equal("Unknown", result.StatusDescription);
-        //     Assert.Equal("INDUCTION", result.CarrierStatusCode);
-        //     Assert.Equal("Electronic information submitted by shipper", result.CarrierStatusDescription);
-        //     Assert.Equal("2021-07-09T07:00:00Z", result.ShipDate);
-        //     Assert.Equal("2021-07-19T07:00:00Z", result.EstimatedDeliveryDate);
-        //     Assert.Null(result.ActualDeliveryDate);
-        //     Assert.Null(result.ExceptionDescription);
+            Assert.Equal("1026167028310292", result.TrackingNumber);
+            Assert.Equal(StatusCode.UN, result.StatusCode);
+            Assert.Null(result.CarrierDetailCode);
+            Assert.Equal(StatusDescription.Unknown, result.StatusDescription);
+            Assert.Equal("INDUCTION", result.CarrierStatusCode);
+            Assert.Equal("Electronic information submitted by shipper", result.CarrierStatusDescription);
+            Assert.Equal("2021-07-09T07:00:00Z", result.ShipDate);
+            Assert.Equal("2021-07-19T07:00:00Z", result.EstimatedDeliveryDate);
+            Assert.Null(result.ActualDeliveryDate);
+            Assert.Null(result.ExceptionDescription);
 
-        //     Assert.Equal("2021-07-10T02:58:01Z", result.Events[0].OccurredAt);
-        //     Assert.Equal("2021-07-10T02:58:01Z", result.Events[0].CarrierOccurredAt);
-        //     Assert.Equal("Electronic information submitted by shipper", result.Events[0].Description);
-        //     Assert.Null(result.Events[0].CityLocality);
-        //     Assert.Null(result.Events[0].StateProvince);
-        //     Assert.Null(result.Events[0].PostalCode);
-        //     Assert.Null(result.Events[0].CountryCode);
-        //     Assert.Null(result.Events[0].CompanyName);
-        //     Assert.Null(result.Events[0].Signer);
-        //     Assert.Equal("3000", result.Events[0].EventCode);
-        //     Assert.Null(result.Events[0].CarrierDetailCode);
-        //     Assert.Equal("", result.Events[0].StatusCode);
-        //     Assert.Equal("3000", result.Events[0].CarrierStatusCode);
-        //     Assert.Null(result.Events[0].Latitude);
-        //     Assert.Null(result.Events[0].Longitude);
-        // }
+            Assert.Equal("2021-07-10T02:58:01Z", result.Events[0].OccurredAt);
+            Assert.Equal("2021-07-10T02:58:01Z", result.Events[0].CarrierOccurredAt);
+            Assert.Equal("Electronic information submitted by shipper", result.Events[0].Description);
+            Assert.Null(result.Events[0].CityLocality);
+            Assert.Null(result.Events[0].StateProvince);
+            Assert.Null(result.Events[0].PostalCode);
+            Assert.Null(result.Events[0].CountryCode);
+            Assert.Null(result.Events[0].CompanyName);
+            Assert.Null(result.Events[0].Signer);
+            Assert.Equal("3000", result.Events[0].EventCode);
+            Assert.Null(result.Events[0].CarrierDetailCode);
+            Assert.Equal("", result.Events[0].StatusCode);
+            Assert.Equal("3000", result.Events[0].CarrierStatusCode);
+            Assert.Null(result.Events[0].Latitude);
+            Assert.Null(result.Events[0].Longitude);
+        }
+
+        [Fact]
+        // Check that both API Key and timeout can be set at the method level
+        public async void ValidateCustomSettingsAtMethodLevel()
+        {
+            var apiKeyString = "TEST_bTYAskEX6tD7vv6u/cZ/M4LaUSWBJ219+8S1jgFcnkk";
+
+            var config = new Config(apiKey: apiKeyString, timeout: TimeSpan.FromSeconds(1));
+
+            var mockHandler = new Mock<ShipEngine>(config);
+
+            var shipEngine = mockHandler.Object;
+            string json = File.ReadAllText(Path.Combine(Directory.GetCurrentDirectory(), "../../../HttpResponseMocks/ListCarriers200Response.json"));
+
+            var listCarriersResult = JsonConvert.DeserializeObject<TrackUsingCarrierCodeAndTrackingNumberResult>(json, TestUtils.JsonSerializerSettings);
+            var request = new HttpRequestMessage(HttpMethod.Get, "v1/carriers");
+
+            // Verify that the client has a custom timeout of 1 second when called.
+            mockHandler
+                .Setup(x => x.SendHttpRequestAsync<TrackUsingCarrierCodeAndTrackingNumberResult>
+                (
+                    It.IsAny<HttpMethod>(),
+                    It.IsAny<string>(),
+                    It.IsAny<string>(),
+                    It.Is<HttpClient>(client =>
+                        client.Timeout == TimeSpan.FromSeconds(1) &&
+                        client.DefaultRequestHeaders.ToString().Contains("12345")),
+                    It.IsAny<Config>()
+                ))
+                .Returns(Task.FromResult(listCarriersResult));
+
+            var customConfig = new Config(apiKey: "12345", timeout: TimeSpan.FromSeconds(1));
+
+            await shipEngine.TrackUsingCarrierCodeAndTrackingNumber("1026167028310292", "usps", methodConfig: customConfig);
+
+            mockHandler.VerifyAll();
+        }
+
+        [Fact]
+        public async void InvalidRetriesInMethodCall()
+        {
+            var apiKeyString = "TEST_bTYAskEX6tD7vv6u/cZ/M4LaUSWBJ219+8S1jgFcnkk";
+
+            var config = new Config(apiKey: apiKeyString);
+            var mockHandler = new Mock<ShipEngine>(config);
+            var shipEngine = mockHandler.Object;
+
+            var ex = await Assert.ThrowsAsync<ShipEngineException>(
+                async () => await shipEngine.TrackUsingCarrierCodeAndTrackingNumber(
+                    "1026167028310292",
+                    "usps",
+                    methodConfig: new Config(apiKey: "12345", retries: -1)
+                )
+            );
+            Assert.Equal(ErrorSource.Shipengine, ex.ErrorSource);
+            Assert.Equal(ErrorType.Validation, ex.ErrorType);
+            Assert.Equal(ErrorCode.InvalidFieldValue, ex.ErrorCode);
+            Assert.Equal("Retries must be greater than zero.", ex.Message);
+            Assert.Null(ex.RequestId);
+        }
     }
 }
