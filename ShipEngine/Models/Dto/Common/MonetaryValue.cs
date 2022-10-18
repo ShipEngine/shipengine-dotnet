83d350f4262f84339cd591d763e528220ee2a867
Brian Prigge <brprigge@gmail.com>
2022-10-18 09:48:15 -0500

    Handle full response object.

--- ShipEngine/Models/Dto/Common/MonetaryValue.cs
+++ ShipEngine/Models/Dto/Common/MonetaryValue.cs
@@ -17,24 +17,46 @@ namespace ShipEngineSDK.Common
         public double? Amount { get; set; }
     }
 
-    public class MonetaryValueConverter : JsonConverter
+    public class MonetaryValueConverter : JsonConverter<MonetaryValue>
     {
         public override bool CanWrite => false;
-        public override bool CanConvert(Type objectType)
-        {
-            return objectType == typeof(MonetaryValue);
-        }
 
-        public override object? ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        public override MonetaryValue ReadJson(JsonReader reader, Type objectType, MonetaryValue existingValue, bool hasExistingValue, JsonSerializer serializer)
         {
-            if (reader.ValueType == typeof(double))
+            if (reader.TokenType == JsonToken.StartObject)
+            {
+                var response = new MonetaryValue();
+                while (reader.TokenType != JsonToken.EndObject)
+                {
+                    reader.Read();
+                    if (reader.TokenType == JsonToken.PropertyName)
+                    {
+                        switch (reader.Value)
+                        {
+                            case "amount":
+                                response.Amount = reader.ReadAsDouble();
+                                break;
+                            case "currency":
+                                if (Enum.TryParse(reader.ReadAsString(), true, out Currency currency))
+                                {
+                                    response.Currency = currency;
+                                }
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+                }
+                return response;
+            }
+            if (reader.TokenType == JsonToken.Float)
             {
                 return new MonetaryValue() { Amount = (double)reader.Value };
             }
-            return null;
+            return new MonetaryValue();
         }
 
-        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        public override void WriteJson(JsonWriter writer, MonetaryValue value, JsonSerializer serializer)
         {
             throw new NotImplementedException();
         }
