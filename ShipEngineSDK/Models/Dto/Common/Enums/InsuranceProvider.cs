403d733cbee447fbe4a299d69542d6cc10de1aad
Andy Visser <visser@gmail.com>
2024-09-09 21:22:09 -0400

    fix: add FundingSource & ParcelGuard as valid options for InsuranceProvider.cs

--- ShipEngineSDK/Models/Dto/Common/Enums/InsuranceProvider.cs
+++ ShipEngineSDK/Models/Dto/Common/Enums/InsuranceProvider.cs
@@ -29,6 +29,18 @@ namespace ShipEngineSDK.Common.Enums
         /// The package is insured by a third-party insurance service, outside of ShipEngine.
         /// </summary>
         [EnumMember(Value = "third_party")]
-        ThirdParty
+        ThirdParty,
+
+        /// <summary>
+        /// This option will assign the insurance provider available with ShipEngine Carriers to the shipment (currently ParcelGuard). The response will auto-populate the insurance_provider field with parcelguard.
+        /// </summary>
+        [EnumMember(Value = "funding_source")]
+        FundingSource,
+
+        /// <summary>
+        /// This option will assign ParcelGuard as the insurance provider for the shipment. Using this value is functionally the same as using the 'funding_source' value.
+        /// </summary>
+        [EnumMember(Value = "parcelguard")]
+        ParcelGuard
     }
 }
